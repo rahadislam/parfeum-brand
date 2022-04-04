3b57827a03a3a754224c0decdd4fe275f0097c00
rahadislam <rahadislm.web@gmil.com>
2022-04-05 01:48:09 +0600

    dynamic load complete

---
+++ src/comoponets/BestReview/BestReview.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const BestReview = (props) => {
+    const {name,img,rating,feedback}=props.feedback;
+    return (
+        <div className='bg-stone-50 p-5 rounded-lg drop-shadow-lg'>
+        <div className='flex items-center my-5'>
+            <img className='w-16 h-16 rounded-full drop-shadow-lg' src={img} alt="" />
+            <span className='ml-4 font-medium'>{name}</span>
+        </div>
+        <p className='mb-5 text-gray-400'>{feedback}</p>
+        <span className='font-bold '>Rating: {rating}</span>
+    </div>
+    );
+};
+
+export default BestReview;
--- src/comoponets/Review/Review.js
+++ src/comoponets/Review/Review.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import useReview from '../../FakeReview/useReview';
+import BestReview from '../BestReview/BestReview';
 
 const Review = () => {
+    const [reviews,setReviews]=useReview({})
     return (
-        <div className='text-center '>
+        <div className='text-center container mx-auto'>
             <h1 className='text-3xl font-bold
-            my-5'>Customars Review </h1>
+            my-5'>Customars Review</h1>
+            <div  className='grid grid-cols-3 gap-8 my-5'>
+            {
+                reviews.slice(0,3).map(review=><BestReview 
+                    key={review.id}
+                    feedback={review}
+                ></BestReview>
+                    )
+            }
+            </div>
+
             <Link to='/reviews' className='my-5 font-bold bg-amber-200 px-5 py-3 rounded-lg'>See All Reviews</Link>
         </div>
     );
